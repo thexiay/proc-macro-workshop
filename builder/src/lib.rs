5d12bd28fb6f5e763d80628863b35e22007f229a
thexia <xiayu1187@gmail.com>
2023-11-12 09:14:57 +0800

    builder test-01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let derive_input = parse_macro_input!(input as DeriveInput);
+    eprintln!("{:#?}", derive_input.into_token_stream());
+    TokenStream::new()
 }
