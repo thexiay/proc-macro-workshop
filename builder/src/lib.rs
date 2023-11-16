f50bf58cba4cbe28ee7ece457e82f81567823d8a
thexia <xiayu1187@gmail.com>
2023-11-17 00:00:47 +0800

    all rest of builder test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,145 +1,270 @@
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
 use quote::{quote, format_ident};
-use syn::{parse_macro_input, DeriveInput, Data, Fields, FieldsNamed, Type, TypePath};
+use syn::{DeriveInput, Data, Fields, Type, TypePath, Meta, MetaNameValue, punctuated::Punctuated, Token, Expr, ExprLit, Lit, spanned::Spanned, parse_macro_input};
+
+struct BuilderContext {
+    name: Ident,
+    fields_ctx: Vec<BuilderFieldCtx>,
+}
+
+enum BuilderFieldCtx {
+    Option(OptionFieldCtx),
+    VecEach(VecEachFieldCtx),
+    // other
+    Other(OtherFieldCtx),
+}
+
+struct OptionFieldCtx {
+    name: Ident,
+    _outter_ty: Type,
+    inner_ty: Type
+}
+
+struct VecEachFieldCtx {
+    name: Ident,
+    each_name: Ident,
+    outter_ty: Type,
+    inner_ty: Type,
+}
+
+struct OtherFieldCtx {
+    name: Ident,
+    ty: Type
+}
+
+impl BuilderFieldCtx {
+    fn struct_filed(&self) -> proc_macro2::TokenStream {
+        match self {
+            BuilderFieldCtx::Option(OptionFieldCtx{ name, inner_ty, ..}) => {
+                quote!(#name: std::option::Option<#inner_ty>)
+            }
+            BuilderFieldCtx::VecEach(VecEachFieldCtx{ name, outter_ty, ..}) => {
+                quote!(#name: #outter_ty)
+            }
+            BuilderFieldCtx::Other(OtherFieldCtx{ name, ty}) => {
+                quote!(#name: std::option::Option<#ty>)
+            }
+        }
+    }
+
+    fn init_filed(&self) -> proc_macro2::TokenStream {
+        match self {
+            BuilderFieldCtx::Option(OptionFieldCtx {name, ..}) => {
+                quote!(#name: std::option::Option::None)
+            }
+            BuilderFieldCtx::VecEach(VecEachFieldCtx {name, ..}) => {
+                quote!(#name: std::vec::Vec::new())
+            }
+            BuilderFieldCtx::Other(OtherFieldCtx {name, ..}) => {
+                quote!(#name: std::option::Option::None)
+            }
+        }
+    }
+
+    fn setter_field(&self) -> proc_macro2::TokenStream {
+        match self {
+            BuilderFieldCtx::Option(OptionFieldCtx {name, inner_ty, ..}) => {
+                quote! {
+                    fn #name(&mut self, #name: #inner_ty) -> &mut Self {
+                        self.#name = std::option::Option::Some(#name);
+                        self
+                    }
+                }
+            }
+            BuilderFieldCtx::VecEach(VecEachFieldCtx{name, each_name, inner_ty, outter_ty, ..}) => {
+                let token = quote! {
+                    fn #each_name(&mut self, #each_name: #inner_ty) -> &mut Self {
+                        self.#name.push(#each_name);
+                        self
+                    }
+                };
+                if name != each_name {
+                    quote! {
+                        fn #name(&mut self, #name: #outter_ty) -> &mut Self {
+                            self.#name.extend(#name);
+                            self
+                        }    
+                        #token
+                    }
+                } else {
+                    token
+                }
+            }
+            BuilderFieldCtx::Other(OtherFieldCtx {name, ty}) => {
+                quote! {
+                    fn #name(&mut self, #name: #ty) -> &mut Self {
+                        self.#name = std::option::Option::Some(#name);
+                        self
+                    }    
+                }
+            }
+        }
+    }
+
+    fn builder_field(&self) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
+        match self {
+            BuilderFieldCtx::Option(OptionFieldCtx { name, .. }) => {
+                (
+                    proc_macro2::TokenStream::new(), 
+                    quote!(#name: self.#name.clone())
+                )
+            }
+            BuilderFieldCtx::VecEach(VecEachFieldCtx{ name, .. }) => {
+                (
+                    proc_macro2::TokenStream::new(),  
+                    quote!(#name: self.#name.clone())
+                )
+            }
+            BuilderFieldCtx::Other(OtherFieldCtx {name, ..}) => {
+                (
+                    quote! {
+                        if self.#name.is_none() {
+                            let err = format!("{} have not set!", stringify!(name));
+                            return std::result::Result::Err(err.into())
+                        }
+                    }, 
+                    quote!(#name: self.#name.clone().unwrap())
+                )
+            }
+        }
+    }
+}
 
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
-    let ident = &derive_input.ident;
-    let attr = derive_input.attrs.first().unwrap();
-    eprintln!("{:#?}", attr);
-    match &derive_input.data {
-        Data::Struct(ref data) => {
-            match data.fields {
-                Fields::Named(ref fields) => {
-                    let builder_struct = build_struct(ident, fields).unwrap();
-                    let builder_construct = build_construct(ident, fields).unwrap();
-                    let builder_setter = build_setter(ident, fields).unwrap();
-                    let builder_builder = build_builder(ident, fields).unwrap();
-                    quote! {
-                        #builder_struct
-                        #builder_setter
-                        #builder_construct
-                        #builder_builder
-                    }.into()
-                }
-                _ => unimplemented!()
-            } 
+    match &build_ctx(&derive_input) {
+        Ok(builder_ctx) => {
+            let builder_struct = build_struct(builder_ctx);
+            let builder_construct = build_construct(builder_ctx);
+            let builder_setter = build_setter(builder_ctx);
+            let builder_builder = build_builder(builder_ctx);
+            quote! {
+                #builder_struct
+                #builder_setter
+                #builder_construct
+                #builder_builder
+            }.into()
         }
-        Data::Union(_) | Data::Enum(_) => unimplemented!(),
+        Err(e) => return e.to_compile_error().into()
     }
 }
 
-fn build_struct(ident: &Ident, fields: &FieldsNamed) -> syn::Result<proc_macro2::TokenStream> {
-    let strcut_fields: Vec<proc_macro2::TokenStream> = fields.named.iter()
-        .map(|f| {
-            let name = &f.ident;
-            let outter_type = &f.ty;
-            if let Some(inner_type) = parse_type(outter_type) {
-                quote!(#name: std::option::Option<#inner_type>)
-            } else {
-                quote!(#name: std::option::Option<#outter_type>)
+fn build_ctx(input: &DeriveInput) -> syn::Result<BuilderContext> {
+    let name = &input.ident;
+    match &input.data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    let fields_ctx_with_err: Vec<syn::Result<BuilderFieldCtx>> = fields.named.iter()
+                        .map(|f | -> syn::Result<BuilderFieldCtx> {
+                            if let Some(inner_type) = parse_inner_type(&f.ty, "Option") {
+                                
+                                return Ok(BuilderFieldCtx::Option(OptionFieldCtx {
+                                    name: f.ident.as_ref().unwrap().clone(),
+                                    _outter_ty: f.ty.clone(),
+                                    inner_ty: inner_type.clone(),
+                                }));
+                            } 
+                            
+                            if let Some(inner_type) = parse_inner_type(&f.ty, "Vec") {
+                                if let Some(name) = parse_helper_each(f)? {
+                                    return Ok(BuilderFieldCtx::VecEach(VecEachFieldCtx { 
+                                        name: f.ident.as_ref().unwrap().clone(),
+                                        each_name: name, 
+                                        outter_ty: f.ty.clone(),
+                                        inner_ty: inner_type.clone(), 
+                                    }));
+                                } 
+                            }
+                            Ok(BuilderFieldCtx::Other(OtherFieldCtx { 
+                                name: f.ident.as_ref().unwrap().clone(),
+                                ty: f.ty.clone(),
+                            }))
+                        }).collect();
+                    let mut fields_ctx = vec![];
+                    for field in fields_ctx_with_err {
+                        fields_ctx.push(field?);
+                    }
+                    Ok(BuilderContext {
+                        name: input.ident.clone(),
+                        fields_ctx,
+                    })
+                    
+                }
+                _ => Err(syn::Error::new_spanned(name, "not fielded names"))
             }
-        }).collect();
-    let strcut_name = format_ident!("{}Builder", ident);
-    let tokens = quote! {
-        pub struct #strcut_name {
-            #(#strcut_fields),*
         }
-    };
-    Ok(tokens)
+        Data::Union(_) | Data::Enum(_) => Err(syn::Error::new_spanned(name, "unimpelement union and enum"))
+    }
+    
 }
 
-fn build_construct(ident: &Ident, fields: &FieldsNamed) -> syn::Result<proc_macro2::TokenStream> {
-    let idents = fields.named.iter().map(|f| &f.ident);
-    let strcut_name = format_ident!("{}Builder", ident);
-    let tokens = quote! {
-        impl #ident {
+fn build_struct(ctx: &BuilderContext) -> proc_macro2::TokenStream {
+    let struct_fields = ctx.fields_ctx.iter().map(|f| f.struct_filed());
+    let strcut_name = format_ident!("{}Builder", ctx.name);
+    quote! {
+        pub struct #strcut_name {
+            #(#struct_fields),*
+        }
+    }
+}
+
+fn build_construct(ctx: &BuilderContext) -> proc_macro2::TokenStream {
+    let init_fields = ctx.fields_ctx.iter().map(|f| f.init_filed());
+    let name = &ctx.name;
+    let strcut_name = format_ident!("{}Builder", ctx.name);
+    quote! {
+        impl #name {
             pub fn builder() -> #strcut_name {
                 #strcut_name {
-                    #(#idents: std::option::Option::None),*
+                    #(#init_fields),*
                 }
             }
         }
-    };
-    Ok(tokens)
+    }
 }
 
-fn build_setter(ident: &Ident, fields: &FieldsNamed) -> syn::Result<proc_macro2::TokenStream> {
-    let mut setter_funcs: Vec<proc_macro2::TokenStream> = vec![];
-    fields.named.iter().for_each(|f| {
-        let name = &f.ident;
-        let outter_type = &f.ty;
-        let token = if let Some(inner_type) = parse_type(outter_type) {
-            quote! {
-                fn #name(&mut self, #name: #inner_type) -> &mut Self {
-                    self.#name = std::option::Option::Some(#name);
-                    self
-                }
-            }
-        } else {
-            quote! {
-                fn #name(&mut self, #name: #outter_type) -> &mut Self {
-                    self.#name = std::option::Option::Some(#name);
-                    self
-                }
-            }
-        };
-        setter_funcs.push(token);
+fn build_setter(ctx: &BuilderContext) -> proc_macro2::TokenStream {
+    let setter_fields = ctx.fields_ctx.iter().map(|f| {
+        let a = f.setter_field();
+        a
     });
-
-    let strcut_name = format_ident!("{}Builder", ident);
-    let tokens = quote! {
+    let strcut_name = format_ident!("{}Builder", ctx.name);
+    quote! {
         impl #strcut_name {
-            #(#setter_funcs)*
+            #(#setter_fields)*
         }
-    };
-    Ok(tokens)
+    }
 }
 
-fn build_builder(ident: &Ident, fields: &FieldsNamed) -> syn::Result<proc_macro2::TokenStream> {
-    let strcut_name = format_ident!("{}Builder", ident);
-    let checker_with_init: Vec<(proc_macro2::TokenStream, proc_macro2::TokenStream)>  = fields.named.iter()
-        .map(|f| {
-            
-            let name = &f.ident;
-            let outter_type = &f.ty;
-            if let None = parse_type(outter_type) {
-                let checker = quote! {
-                    if self.#name.is_none() {
-                        let err = format!("{} have not set!", stringify!(name));
-                        return std::result::Result::Err(err.into())
-                    }
-                };
-                let init = quote!(#name: self.#name.clone().unwrap());
-                (checker, init)
-            } else {
-                (proc_macro2::TokenStream::new(), quote!(#name: self.#name.clone()))
-            } 
-        }).collect();
-    let checkers = checker_with_init.iter().map(|ci| &ci.0);
-    let inits = checker_with_init.iter().map(|ci| &ci.1);
-    let tokens = quote! {
+fn build_builder(ctx: &BuilderContext) -> proc_macro2::TokenStream {
+    let name = &ctx.name;
+    let strcut_name = format_ident!("{}Builder", ctx.name);
+    let build_fileds = ctx.fields_ctx.iter().map(|f| f.builder_field());
+    let checker_code = build_fileds.clone().map(|f|f.0);
+    let build_code = build_fileds.map(|f|f.1);
+    quote! {
         extern crate alloc;
         impl #strcut_name {
-            pub fn build(&mut self) -> std::result::Result<#ident, Box<dyn std::error::Error>> {
-                #(#checkers)*
+            pub fn build(&mut self) -> std::result::Result<#name, std::boxed::Box<dyn std::error::Error>> {
+                #(#checker_code)*
 
-                Ok(#ident {
-                    #(#inits),*
+                Ok(#name {
+                    #(#build_code),*
                 })
             }
         }
-    };
-    Ok(tokens)
+    }
 }
 
-fn parse_type(ty: &Type) -> Option<&Type> {
+// parse inner type. only support 'vec' and 'option'
+fn parse_inner_type<'a>(ty: &'a Type, outter_type: &str) -> Option<&'a Type> {
     if let Type::Path(TypePath {ref path, ..}) = ty {
         if let Some(seg) = path.segments.last() {
-            if seg.ident == "Option" {
+            if seg.ident == outter_type {
                 if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
                     ref args,
                     ..
@@ -153,4 +278,38 @@ fn parse_type(ty: &Type) -> Option<&Type> {
         }
     }
     None
+}
+
+/// support attribute: #[helper(each = "xxx")] on vec data type
+fn parse_helper_each(field: &syn::Field) -> syn::Result<Option<Ident>> {
+    // find first helper attribute
+    if let Some(attr) = field.attrs.first() {
+        let mut each_parse_res = None;
+        if attr.path().is_ident("builder") {
+            let nested = attr.parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)?;
+            for meta in nested {
+                match &meta {
+                    Meta::NameValue(MetaNameValue {
+                        path,
+                        value: Expr::Lit(ExprLit{ 
+                            lit: Lit::Str(str),
+                            ..
+                        }),
+                        ..
+                    }) if path.is_ident("each") => {
+                        each_parse_res = Some(Ident::new(str.value().as_str(), str.span()));
+                    }
+                    _ => ()
+                }
+            }
+        } 
+        
+        if let Some(each_value) = each_parse_res {
+            Ok(Some(each_value))
+        } else {
+            Err(syn::Error::new(attr.meta.span(), "expected `builder(each = \"...\")`"))
+        }
+    } else {
+        Ok(None)
+    }
 }
--- main.rs
+++ main.rs
@@ -1,9 +1,47 @@
-// Write code here.
-//
-// To see what the code looks like after macro expansion:
-//     $ cargo expand
-//
-// To run the code:
-//     $ cargo run
-
+#![feature(prelude_import)]
+#[prelude_import]
+use std::prelude::rust_2021::*;
+#[macro_use]
+extern crate std;
+use derive_builder::Builder;
+type Option = ();
+type Some = ();
+type None = ();
+type Result = ();
+pub struct Command {
+    executable: String,
+}
+pub struct CommandBuilder {
+    executable: std::option::Option<String>,
+}
+impl CommandBuilder {
+    fn executable(&mut self, executable: String) -> &mut Self {
+        self.executable = std::option::Option::Some(executable);
+        self
+    }
+}
+impl Command {
+    pub fn builder() -> CommandBuilder {
+        CommandBuilder {
+            executable: std::option::Option::None,
+        }
+    }
+}
+extern crate alloc;
+impl CommandBuilder {
+    pub fn build(&mut self) -> std::result::Result<Command, std::boxed::Box<dyn std::error::Error>> {
+        if self.executable.is_none() {
+            let err = {
+                let res = ::alloc::fmt::format(
+                    format_args!("{0} have not set!", "name"),
+                );
+                res
+            };
+            return std::result::Result::Err(err.into());
+        }
+        Ok(Command {
+            executable: self.executable.clone().unwrap(),
+        })
+    }
+}
 fn main() {}
